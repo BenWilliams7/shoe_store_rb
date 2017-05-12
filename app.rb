787e40ba6506123f343efc1a7e496fbb1716e494
Ben Williams <benjarwilliams@gmail.com>
2017-05-12 14:24:16 -0700

    expand on update/delete infrastructure

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ get "/" do
   erb :index
 end
 
+# store addition and display
 get('/stores') do
   @store_message = Store.all.length > 0 ? "Select a store" : "Add a store below"
   erb :shoe_stores
@@ -16,6 +17,48 @@ end
 
 post "/stores" do
   store_name = params.fetch("store-name")
-  Store.create({name: store_name})
+  @store = Store.create({name: store_name})
   redirect "/stores"
+end
+
+get "store/:id" do
+  @store = Store.find(params['id'].to_i)
+  erb :shoe_stores
+end
+
+# Update and delete stores
+
+delete '/store/delete/:id' do
+  store = Store.find(params['id'].to_i)
+  store.delete
+  redirect '/'
+end
+
+get '/store/edit/:id' do
+  @store = Store.find(params['id'].to_i)
+  erb :shoe_store_edit
+end
+
+patch '/rename_store/:id' do
+  @store = Store.find(params['id'].to_i)
+  name = params['name']
+  @store.update(name: name)
+  # logo = params['logo']
+  # @store.brands.first.update(logo: logo)
+  @store = Store.find(params['id'].to_i)
+  if @store.update(name: name)
+    erb :store
+  end
+
+# brand addition and display
+
+get('/brands') do
+  @brand_message = Brand.all.length > 0 ? "Select a brand" : "Add a brand below"
+  erb :shoe_brands
+end
+
+post "/brands" do
+  brand_name = params.fetch("brand-name")
+  Brand.create({logo: brand_name})
+  redirect "/brands"
 end
